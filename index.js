8b4c8a94d2a1c40c4a09e0dcd382d82ab20670f1
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-15 12:18:21 +0100

    commit tree 8c31eccea0a46246aabe92e0e0075b31f8e15548

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-
 // Write your code here
+
+/*
+const num1 = 2; 
+const num2 = 31;
+
+return num1 * num2;
+*/
+
+
+let num1= 2;
+let num2 = 31;
+
+let multiply = num1*num2;
+
+multiply;
+
+/* smarter way of doing the exact same thing as above: 
+
+let num1= 2;
+let num2 = 31;
+let multiply; 
+
+multiply = num1*num2;
+
+multiply;
+
+*/
+
+let random = Math.floor(Math.random()*10)+1;
+random;
+  
+// ...... passed test 2 following similar example from the lesson 
+
+let num3 = 100
+let num4 = 2
+
+let mod = Number.parseInt(num3, num4);
+mod;
+
+// very weird way to pass that test, i think. 
+
+let max = Math.max(1, 2, 20, 17, 3);
+max;l
